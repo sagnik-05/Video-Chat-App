5f00c59b94d614978218ab2b6159cc9f6ba32229
aman08-cool <aman.12100190@lpu.in>
2022-07-08 19:59:48 +0530

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import VideoPlayer from './components/VideoPlayer'
-import Notifications from './components/Notifications'
-import Options from './components/Options'
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import VideoPlayer from "./components/VideoPlayer";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
 
 const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
 
-  }));
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
 
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.wrapper}>
       <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
-      </AppBar> 
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
       <VideoPlayer />
-  
-      <Options> 
-         <Notifications />
+
+      <Options>
+        <Notifications />
       </Options>
     </div>
   );
-}
+};
 
-export default App
+export default App;
